2759da48bc595cd1f2000c7628f8c7de1e99a0f0
sakalesha <ronadasakalesha@gmail.com>
2025-12-25 22:15:55 +0530

    Resolved secrates for security

--- notifier.py
+++ notifier.py
@@ -1,11 +1,18 @@
 import asyncio
 from telegram import Bot
 import os
+from dotenv import load_dotenv
 
-BOT_TOKEN = "7959899285:AAEYQH_kyc_1i7j7LA85PaFcFKeRP7PbMcs"
-CHAT_ID = "6640875299"
+load_dotenv()
+
+BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
 
 async def _send_async(msg):
+    if not BOT_TOKEN or not CHAT_ID:
+        print("Error: Bot token or Chat ID not found in environment variables.")
+        return
+        
     bot = Bot(token=BOT_TOKEN)
     await bot.send_message(chat_id=CHAT_ID, text=msg)
 
